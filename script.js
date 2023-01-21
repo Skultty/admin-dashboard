16c0ce32ecdd38ee2146f290a2087b96c7fda19a
Skultty <samuel.skultety@outlook.sk>
2023-01-21 14:47:32 +0100

    feat: basic layout and styles for the sidebar

---
+++ script.js
@@ -0,0 +1,8 @@
+// event listener for sidebar toggle between active and hidden classes
+document.getElementById('sidebarToggle').addEventListener('click', function() {
+  document.getElementById('sidebar').classList.toggle('active');
+  // from all elements that have sidebarText class toggle hiddenText class
+    document.querySelectorAll('.sidebarText').forEach(function(element) {
+      element.classList.toggle('hiddenText');
+    }); 
+});
